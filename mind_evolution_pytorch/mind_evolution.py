e03c0a888892fa5f22bd4a876214d5d25cff703e
lucidrains <lucidrains@gmail.com>
2025-01-22 06:23:37 -0800

    go top down

--- mind_evolution_pytorch/mind_evolution.py
+++ mind_evolution_pytorch/mind_evolution.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
 import torch
-from torch import nn
+from torch import nn, Tensor
 from torch.nn import Module, ModuleList
 import torch.nn.functional as F
+
+# constants
+
+Population = list[Tensor]
+Islands = list[Population]
 
 # helper functions
 
@@ -10,6 +17,47 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+# operators
+
+def init_islands(
+    model: Module,
+    num_islands = 1
+) -> Islands:
+    raise NotImplementedError
+
+def migration(
+    islands: Islands
+) -> Islands:
+    raise NotImplementedError
+
+def reset_island(
+    islands: Islands
+) -> Islands:
+    raise NotImplementedError
+
+def refinement(
+    critic: Module,
+    solution: Tensor
+) -> Tensor:
+    raise NotImplementedError
+
+def mutation(
+    solution: Tensor
+) -> Tensor:
+    raise NotImplementedError
+
+def tournament_selection(
+    population: Population,
+    num_tournaments: int
+) -> tuple[Populations, ...]:
+    raise NotImplementedError
+
+def recombination(
+    solution1: Tensor,
+    solution2: Tensor
+) -> Tensor:
+    raise NotImplementedError
 
 # main class
 
